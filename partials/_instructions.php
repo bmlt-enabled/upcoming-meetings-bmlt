204a57757056b9dfece2dac8ed5d749e5f8da315
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2020-01-11 09:40:35 -0500

    use wordpress set timezone by default (#15)

--- partials/_instructions.php
+++ partials/_instructions.php
@@ -3,7 +3,7 @@
 <div id="upcoming_meetings_accordion">
     <h3 class="help-accordian"><strong>Basic</strong></h3>
     <div>
-        <p>[upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;12&quot; timezone="America/New_York]</p>
+        <p>[upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;12&quot;]</p>
         <p>Multiple service bodies can be added seperated by a comma like so services=&quot;12,14,15&quot;</p>
         <strong>Attributes:</strong> root_server, services, recursive, grace_period, num_results, display_type, timezone, location_text, time_format, weekday_language
         <p><strong>Shortcode parameters can be combined.</strong></p>
@@ -21,8 +21,8 @@
         <p><strong>location_text</strong></p>
         <p><strong>time_format</strong></p>
         <p><strong>weekday_language</strong></p>
-        <p>A minimum of root_server, services and timezone attribute are required, which would return all towns for that service body seperated by a comma.</p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone="America/New_York"]</p>
+        <p>A minimum of root_server, and services attribute are required, which would return all towns for that service body seperated by a comma.</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot;]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- root_server</strong></h3>
     <div>
@@ -34,54 +34,54 @@
     <div>
         <p><strong>services (required)</strong></p>
         <p>The Service Body ID of the service body you would like to include, to add multiple service bodies just seperate by a comma..</p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; timezone="America/New_York" services=&quot;50,37,26&quot;]</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50,37,26&quot;]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- recursive</strong></h3>
     <div>
         <p><strong>recursive</strong></p>
         <p>To recurse service bodies add recursive=&quot;1&quot;. This can be useful when using a Service Body Parent ID</p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone="America/New_York" recursive=&quot;1&quot;]</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; recursive=&quot;1&quot;]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- grace_period</strong></h3>
     <div>
         <p><strong>grace_period</strong></p>
         <p>To add a grace period to meeting lookup add grace_period=&quot;15&quot; this would add a 15 minute grace period.</p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone="America/New_York" grace_period=&quot;15&quot;]</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; grace_period=&quot;15&quot;]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- num_results</strong></h3>
     <div>
         <p><strong>num_results</strong></p>
         <p>To limit the number of results add num_results="5" this would limit results to 5.</p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; state=&quot;1&quot; timezone="America/New_York" num_results=&quot;5&quot;]</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; state=&quot;1&quot; num_results=&quot;5&quot;]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- display_type</strong></h3>
     <div>
         <p><strong>display_type</strong></p>
         <p>To change the display type add display_type="table" there are three different types <strong>simple</strong>, <strong>table</strong>, <strong>block</strong></p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone="America/New_York" display_type=&quot;table"]</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; display_type=&quot;table"]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- timezone</strong></h3>
     <div>
         <p><strong>timezone</strong></p>
-        <p>This is required and should be set to what timezones your meetings are in, We can not rely on servers time zone. add timezone="America/New_York" you can set this in the admin setting or short code. A complete list of timezones can be found here http://php.net/manual/en/timezones.php</p>
+        <p>By default we use your WordPress sites timezone setting, this will overwrite that. add timezone="America/New_York" you can set this in the admin setting or short code. A complete list of timezones can be found here http://php.net/manual/en/timezones.php</p>
         <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone="America/New_York" ]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- location_text</strong></h3>
     <div>
         <p><strong>location_text</strong></p>
         <p>To display the location name using the simple display add location_text="1"</p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone="America/New_York" location_text=&quot;1&quot;]</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; location_text=&quot;1&quot;]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- time_format</strong></h3>
     <div>
         <p><strong>time_format</strong></p>
         <p>This allows you to be able to switch between 12 and 24 hour. the default is 12. To switch to 24 hour add time_format="24"</p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone="America/New_York" time_format=&quot;24"]</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; time_format=&quot;24"]</p>
     </div>
     <h3 class="help-accordian"><strong>&nbsp;&nbsp;&nbsp;&nbsp;- weekday_language</strong></h3>
     <div>
         <p><strong>weekday_language</strong></p>
         <p>This allows you to change the language of the weekday names. To change language to danish set weekday_language="dk". Currently supported languages are Danish and English, the default is English.</p>
-        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone="America/New_York" weekday_language=&quot;dk"]</p>
+        <p>Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; weekday_language=&quot;dk"]</p>
     </div>
 </div>
--- readme.txt
+++ readme.txt
@@ -5,8 +5,8 @@ Plugin URI: https://wordpress.org/plugins/upcoming-meetings-bmlt/
 Tags: bmlt, basic meeting list toolbox, Upcoming Meetings, Upcoming Meetings BMLT, narcotics anonymous, na
 Requires at least: 4.0
 Requires PHP: 5.6
-Tested up to: 5.3
-Stable tag: 1.2.4
+Tested up to: 5.3.2
+Stable tag: 1.2.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -22,36 +22,36 @@ Attributes: root_server, services, recursive, grace_period, num_results, display
 
 == Usage ==
 
-A minimum of root_server, services and timezone attributes are required, which would return the next 5 meetings in simple view with a 15minute grace period.
+A minimum of root_server, and services attributes are required, which would return the next 5 meetings in simple view with a 15minute grace period.
 
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone=&quot;America/New_York&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot;]
 
 **recursive** to recurse service bodies add recursive=&quot;1&quot;
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone=&quot;America/New_York&quot; recursive=&quot;1&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; recursive=&quot;1&quot;]
 
 **services** to add multiple service bodies just seperate by a comma.
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50,37,26&quot; timezone=&quot;America/New_York&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50,37,26&quot;]
 
 **grace_period** To add a grace period to meeting lookup add grace_period=&quot;15&quot; this would add a 15 minute grace period.
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone=&quot;America/New_York&quot; grace_period=&quot;15&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; grace_period=&quot;15&quot;]
 
 **num_results** To limit the number of results add num_results=&quot;5&quot; this would limit results to 5.
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone=&quot;America/New_York&quot; state=&quot;1&quot; num_results=&quot;5&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; state=&quot;1&quot; num_results=&quot;5&quot;]
 
 **display_type** To change the display type add display_type=&quot;table&quot; there are three different types **simple**, **table**, **block**
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; timezone=&quot;America/New_York&quot; display_type=&quot;table&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; display_type=&quot;table&quot;]
 
-**timezone** This is required and should be set to what timezones your meetings are in, We can not rely on servers time zone. add timezone=&quot;America/New_York&quot; you can set this in the admin setting or short code. A complete list of timezones can be found here http://php.net/manual/en/timezones.php
+**timezone** By default we use your WordPress sites timezone setting, this will overwrite that. add timezone=&quot;America/New_York&quot; you can set this in the admin setting or short code. A complete list of timezones can be found here http://php.net/manual/en/timezones.php
 Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; timezone=&quot;America/New_York&quot;]
 
 **location_text** to display the location nam,e using the simple display add location_text=&quot;1&quot;
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; timezone=&quot;America/New_York&quot; location_text=&quot;1&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; services=&quot;50&quot; location_text=&quot;1&quot;]
 
 **time_format** This allows you to be able to switch between 12 and 24 hour. the default is 12. To switch to 24 hour add time_format=&quot;24&quot;
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; timezone=&quot;America/New_York&quot; time_format=&quot;24&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; time_format=&quot;24&quot;]
 
 **weekday_language** This allows you to change the language of the weekday names. To change language to danish set weekday_language=&quot;dk&quot;. Currently supported languages are Danish and English, the default is English.
-Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; timezone=&quot;America/New_York&quot; weekday_language=&quot;dk&quot;]
+Ex. [upcoming_meetings root_server=&quot;https://www.domain.org/main_server&quot; weekday_language=&quot;dk&quot;]
 
 == EXAMPLES ==
 
